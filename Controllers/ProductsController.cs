ed218e6f6cfe8dcec5b27829e7fb9d421b048e82
Stephware <pinacate.stephen@gmail.com>
2026-09-03 18:40:58 +0800

    Initial ASP.NET Core Web API setup

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using MyWebApi.Models;
+
+namespace MyWebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private static readonly List<Product> _products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Product 1", Price = 10.99m },
+        new Product { Id = 2, Name = "Product 2", Price = 19.99m },
+        new Product { Id = 3, Name = "Product 3", Price = 5.99m }
+    };
+
+    [HttpGet]
+    public IActionResult GetAll() => Ok(_products);
+
+    [HttpGet ("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+            return NotFound();
+
+        return Ok(product);
+    }
+}
