8f622b32826522237261aa941d8d846ab70eb3f3
Stephware <pinacate.stephen@gmail.com>
2026-09-03 19:07:48 +0800

    update

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,6 +3,16 @@ namespace MyWebApi.Models;
 public class Product
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Sku { get; set; } = string.Empty;
     public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public bool InStock => StockQuantity > 0;
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public List <tags> Tags { get; set; } = new List<tags>();
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime? Updated { get; set; }
 }
