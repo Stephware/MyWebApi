89b062d644c32c6b2a9a44df413e594608d0f392
Stephware <pinacate.stephen@gmail.com>
2026-09-10 19:48:45 +0800

    Fix product query parameter namespace and property naming

--- DTOs/ProductQueryParameters.cs
+++ DTOs/ProductQueryParameters.cs
@@ -1,9 +1,10 @@
-namespace WebApi.DTOs;
+namespace MyWebApi.DTOs;
 
 public class ProductQueryParameters
 {
     public const int MaxPageSize = 50;
     private int _pageSize = 10;
+
     public int PageNumber { get; set; } = 1;
 
     public int PageSize
@@ -12,19 +13,12 @@ public class ProductQueryParameters
         set => _pageSize = value < 1 ? 10 : Math.Min(value, MaxPageSize);
     }
 
-    public string? search { get; set; }
-
+    public string? Search { get; set; }
     public int? CategoryId { get; set; }
-
     public decimal? MinPrice { get; set; }
-
     public decimal? MaxPrice { get; set; }
-
     public bool? InStockOnly { get; set; }
-
     public bool? IsActive { get; set; }
-
     public string? SortBy { get; set; } = "name";
-
     public bool SortDescending { get; set; } = false;
 }
